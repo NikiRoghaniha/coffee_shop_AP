cf0f5af7cc2fc827bca079bcb26af8a2e01caf68
Niki <niki.roghani@gmail.com>
2024-07-02 19:47:26 +0330

    use simple jwt views for authentication

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from api.views import CreateUserView
 
 urlpatterns = [
     path('user/', CreateUserView.as_view(), name='create-user'),
+    path('token/obtain/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
