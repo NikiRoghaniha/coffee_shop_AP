9862b538d413f5d203a93d7e3ea8535d49325e15
Niki <niki.roghani@gmail.com>
2024-07-03 18:53:32 +0330

    add Product model with relation to Storage via Ingredient model

--- api/models.py
+++ api/models.py
@@ -19,3 +19,21 @@ class Storage(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(null=True)
+    image = models.ImageField(null=True)
+    price = models.PositiveIntegerField()
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.PROTECT)
+    ingredients = models.ManyToManyField('Storage', through='Ingredient')
+
+    def __str__(self):
+        return self.name
+
+
+class Ingredient(models.Model):
+    storage = models.ForeignKey(Storage, on_delete=models.PROTECT)
+    product = models.ForeignKey(Product, on_delete=models.PROTECT)
+    quantity = models.PositiveIntegerField()
