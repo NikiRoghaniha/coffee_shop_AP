0f9aee8ca8b308375c8bf772bb670bfeb59afae0
Niki <niki.roghani@gmail.com>
2024-07-03 16:36:56 +0330

    add category model

--- api/models.py
+++ api/models.py
@@ -4,3 +4,10 @@ from django.db import models
 
 class User(AbstractUser):
     phone_number = models.CharField(max_length=11, unique=True)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
